c588ff617c3d986272704a86ad8ee632cdb2d9b7
unknown <janhavibhudke@gmail.com>
2025-11-14 20:34:30 +0530

    port updated

--- backend/server.js
+++ backend/server.js
@@ -254,5 +254,5 @@ app.get("*", (req, res) => {
 });
 
 // START SERVER
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
